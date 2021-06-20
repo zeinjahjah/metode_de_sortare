3748989954ae94a60fa8060757fb469986297787
zeinjahjah <80286342+zeinjahjah@users.noreply.github.com>
2021-06-20 23:37:32 +0300

    Update metode_de_sortare.py

--- metode_de_sortare.py
+++ metode_de_sortare.py
@@ -32,24 +32,6 @@ def selection_sort(L):
         L[i], L[min_index] = L[min_index], L[i]
     print(L)
 
-#####################
-# def selectionSort(array):
-#     n = len(array)
-#     for i in range(n):
-#         # Initially, assume the first element of the unsorted part as the minimum.
-#         minimum = i
-#
-#         for j in range(i + 1, n):
-#             if (array[j] < array[minimum]):
-#                 # Update position of minimum element if a smaller element is found.
-#                 minimum = j
-#
-#         # Swap the minimum element with the first element of the unsorted part.
-#         temp = array[i]
-#         array[i] = array[minimum]
-#         array[minimum] = temp
-#
-#     return array
 
 #####################
 
@@ -132,7 +114,7 @@ def experimenta (aux):
     arr3 = []
     arr4 = []
     arr5 = []
-    arr6 =[]
+    arr6 = []
     arr7 = []
     arr8 = []
     arr9 = []
